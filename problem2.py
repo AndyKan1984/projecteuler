cefe9c8246a348d0aeb1b3c6e7389e8cea07f974
AndyKan1984 <andishe.kanani@gmail.com>
2022-12-15 11:27:26 +0100

    Minor clean up

--- problem2.py
+++ problem2.py
@@ -1,12 +1,12 @@
-def problem2(max_int):
+def sum_evens_in_fibonacci(max_int):
     first = 1
     second = 2
-    sum_event = 0
+    sum_evens = 0
     while (second < max_int):
         if (second % 2 == 0):
-            sum_event += second
+            sum_evens += second
         next_fib = first+second
         first = second
         second = next_fib
 
-    return sum_event
+    return sum_evens
--- test_promblem2.py
+++ test_promblem2.py
@@ -1,13 +1,17 @@
-from problem2 import problem2
+from problem2 import sum_evens_in_fibonacci
 
 
 def test_problem2_with10() -> None:
-    assert problem2(10) == 10
+    assert sum_evens_in_fibonacci(10) == 10
 
 
 def test_problem2_with20() -> None:
-    assert problem2(20) == 10
+    assert sum_evens_in_fibonacci(20) == 10
+
+
+def test_problem2_with40() -> None:
+    assert sum_evens_in_fibonacci(40) == 44
 
 
 def test_problem2_with4M() -> None:
-    assert problem2(4000000) == 4613732
+    assert sum_evens_in_fibonacci(4_000_000) == 4_613_732
